68b054c554bf7252ba3c09638c0fecbe5d7686d1
M <48407846+mvahaste@users.noreply.github.com>
2023-05-20 15:08:44 +0300

    *

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import { BookCard } from "@/components/ui/book-card";
-import { BookCarousel } from "@/components/ui/book-carousel";
+import { BookGrid } from "@/components/ui/book-carousel";
 import { books } from "@/lib/books";
 
 export default function Home() {
 	return (
 		<main className="flex flex-col p-4 w-full md:max-w-5xl mx-auto">
-			<BookCarousel>
+			<BookGrid>
 				{books.map((book) => (
 					<BookCard book={book} key={book.id} />
 				))}
-			</BookCarousel>
+			</BookGrid>
 		</main>
 	);
 }
--- components/ui/book-carousel.tsx
+++ components/ui/book-carousel.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
-export function BookCarousel({ children }: { children: ReactNode }) {
-	return <div className="grid grid-cols-2 gap-4 md:grid-cols-4">{children}</div>;
+export function BookGrid({ children }: { children: ReactNode }) {
+	return <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4">{children}</div>;
 }
